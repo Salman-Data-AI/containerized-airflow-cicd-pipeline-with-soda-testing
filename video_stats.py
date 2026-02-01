428a96aac73dfa3d38d9c8b7a60c8f1dd131deab
Salman-Data-AI <dexpertkhan@gmail.com>
2026-02-02 00:12:19 +0530

    added function to get video-specific data and cleaned up older files

--- video_stats.py
+++ video_stats.py
@@ -61,13 +61,50 @@ def get_video_id_list(playlist_id):
     except requests.exceptions.RaiseException as e:
         raise e
 
-def video_batch_list(video_id_list, batch_size):
+
+def _video_batch_list(video_id_list, batch_size):
     for video_id in range(0, len(video_id_list), batch_size):
         yield video_id_list[video_id : video_id + batch_size]
+
+def extract_video_data(video_ids):
+    extracted_data = []
+
+    try:
+        for batch in _video_batch_list(video_ids, MAX_RESULTS):
+            video_ids_str = ",".join(batch)
+            batch_url = f"https://youtube.googleapis.com/youtube/v3/videos?part=contentDetails&part=snippet&part=statistics&id={video_ids_str}&key={API_KEY}"
+
+            response = requests.get(batch_url)
+            response.raise_for_status()
+            data = response.json()
+
+            for item in data.get('items', []):
+                video_id = item["id"]
+                snippet = item["snippet"]
+                contentDetails = item['contentDetails']
+                statistics = item['statistics']
+            
+                video_data = {
+                    "video_id": video_id,
+                    "title": snippet['title'],
+                    "publishedAt": snippet.get('publishedAt', None),
+                    "duration": contentDetails['duration'],
+                    "viewCount": statistics.get('viewCount', None),
+                    "likeCount": statistics.get('likeCount', None),
+                    "commentCount": statistics.get('commentCount', None)
+                }
+
+                extracted_data.append(video_data)
+
+        return extracted_data
+
+    except requests.exceptions.RequestException as e:
+        raise e
+
 
 if __name__ == "__main__":
     playlist_id = get_playlist_id()
     video_id_list = get_video_id_list(playlist_id) 
-    for batch in video_batch_list(video_id_list, 10):
-        print(batch)
+    extracted_data = extract_video_data(video_id_list)
+    print(extracted_data)
 

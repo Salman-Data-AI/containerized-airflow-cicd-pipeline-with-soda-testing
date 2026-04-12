e60860622cb16287a2667973b1bc3871a49ed648
Salman-Data-AI <dexpertkhan@gmail.com>
2026-04-12 15:29:05 +0530

    Refactor dags to run sequentially - Scheduling change

--- dags/main.py
+++ dags/main.py
@@ -1,6 +1,7 @@
 from airflow import DAG
 import pendulum
 from datetime import datetime, timedelta
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 
 from api.video_stats import get_playlist_id, get_video_id_list, extract_video_data, save_to_json 
 from datawarehouse.dwh import staging_table, core_table
@@ -42,14 +43,19 @@ with DAG(
     extract_data = extract_video_data(video_ids)
     save_to_json_task = save_to_json(extract_data)
 
+    trigger_update_db = TriggerDagRunOperator(
+        task_id="trigger_update_db",
+        trigger_dag_id="update_db",
+    )
+
     # Define dependencies
-    playlist_id >> video_ids >> extract_data >> save_to_json_task
+    playlist_id >> video_ids >> extract_data >> save_to_json_task >> trigger_update_db
 
 with DAG(
     dag_id = "update_db",
     default_args = default_args,
     description = "DAG to process JSON file & insert data into staging & core schemas",
-    schedule = "0 15 * * *",
+    schedule = None,
     catchup = False
 ) as dag_update:
     
@@ -57,15 +63,20 @@ with DAG(
     update_staging = staging_table()
     update_core = core_table()
 
+    trigger_data_quality = TriggerDagRunOperator(
+        task_id="trigger_data_quality",
+        trigger_dag_id="data_quality",
+    )
+
     # Define dependencies
-    update_staging >> update_core
+    update_staging >> update_core >> trigger_data_quality
     
 # DAG 3: data_quality
 with DAG(
     dag_id="data_quality",
     default_args=default_args,
     description="DAG to check the data quality on both layers in the database",
-    schedule = "0 16 * * *",
+    schedule = None,
     catchup=False
 ) as dag_quality:
 
